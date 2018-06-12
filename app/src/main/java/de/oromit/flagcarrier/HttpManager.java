aeadb673e096d840f13c0838b3273d526c513925
BtbN <btbn@btbn.de>
2018-06-12 15:13:57 +0200

    Refacor login code into own class

--- app/src/main/java/de/oromit/flagcarrier/HttpManager.java
+++ app/src/main/java/de/oromit/flagcarrier/HttpManager.java
@@ -1,4 +1,85 @@
 package de.oromit.flagcarrier;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Map;
+
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+
 class HttpManager {
+    private static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+
+    static class MissingSettingException extends Exception {
+        MissingSettingException(String which) {
+            super("Missing setting: " + which);
+        }
+    }
+
+    static class HttpManagerException extends Exception {
+        HttpManagerException(String msg) {
+            super(msg);
+        }
+    }
+
+    private OkHttpClient mHttpClient = new OkHttpClient();
+    private Callback mCallback;
+    private Context mContext;
+
+    public HttpManager(Context context, Callback callback) {
+        mContext = context;
+        mCallback = callback;
+    }
+
+    public void doRequest(String action, Map<String, String> tagData) throws MissingSettingException, HttpManagerException {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(mContext);
+        String url = prefs.getString("target_url", null);
+
+        if(url == null || url.trim().isEmpty())
+            throw new MissingSettingException("URL");
+
+        JSONObject json = buildJson(action, tagData, prefs);
+
+        postJson(url, json);
+    }
+
+    private JSONObject buildJson(String action, Map<String, String> tagData, SharedPreferences prefs) throws HttpManagerException {
+        String device_id = prefs.getString("device_id", "");
+        String group_id = prefs.getString("group_id", "");
+
+        try {
+            JSONObject json = new JSONObject();
+            JSONObject tag_data = new JSONObject();
+            json.put("action", action);
+            json.put("device_id", device_id);
+            json.put("group_id", group_id);
+
+            for (Map.Entry<String, String> entry : tagData.entrySet()) {
+                tag_data.put(entry.getKey(), entry.getValue());
+            }
+
+            json.put("tag_data", tag_data);
+
+            return json;
+        } catch(JSONException e) {
+            throw new HttpManagerException("JSON error: " + e.getMessage());
+        }
+    }
+
+    private void postJson(String url, JSONObject json) {
+        RequestBody body = RequestBody.create(JSON, json.toString());
+        Request req = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+        mHttpClient.newCall(req).enqueue(mCallback);
+    }
 }
--- app/src/main/java/de/oromit/flagcarrier/LoginActivity.java
+++ app/src/main/java/de/oromit/flagcarrier/LoginActivity.java
@@ -1,35 +1,28 @@
 package de.oromit.flagcarrier;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.nfc.NdefMessage;
 import android.nfc.NfcAdapter;
 import android.os.Parcelable;
-import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.MediaType;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
 import okhttp3.Response;
 import okhttp3.ResponseBody;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements Callback {
     private static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
-    OkHttpClient httpClient = new OkHttpClient();
+    HttpManager httpManager;
     Map<String, String> tagData;
 
     @Override
@@ -37,6 +30,8 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         setTitle(R.string.login_title);
+
+        httpManager = new HttpManager(this, this);
 
         Button b = findViewById(R.id.leftButton);
         b.setOnClickListener(v -> onDoLogin("left"));
@@ -77,38 +72,23 @@ public class LoginActivity extends AppCompatActivity {
         StringBuilder bldr = new StringBuilder();
         StringBuilder other = new StringBuilder();
 
+        HashMap<String, String> knownIdx = new HashMap<>();
+        knownIdx.put("display_name", "Display Name: ");
+        knownIdx.put("country_code", "Country Code: ");
+        knownIdx.put("speedruncom_name", "speedrun.com Name: ");
+        knownIdx.put("twitch_name", "Twitch Name: ");
+        knownIdx.put("twitter_handle", "Twitter Handle: ");
+
         for (Map.Entry<String, String> entry : tagData.entrySet()) {
-            switch(entry.getKey()) {
-                case "display_name":
-                    bldr.append("Display Name: ");
-                    bldr.append(entry.getValue());
-                    bldr.append("\n");
-                    break;
-                case "country_code":
-                    bldr.append("Country Code: ");
-                    bldr.append(entry.getValue());
-                    bldr.append("\n");
-                    break;
-                case "speedruncom_name":
-                    bldr.append("speedrun.com Name: ");
-                    bldr.append(entry.getValue());
-                    bldr.append("\n");
-                    break;
-                case "twitch_name":
-                    bldr.append("Twitch Name: ");
-                    bldr.append(entry.getValue());
-                    bldr.append("\n");
-                    break;
-                case "twitter_handle":
-                    bldr.append("Twitter Handle: ");
-                    bldr.append(entry.getValue());
-                    bldr.append("\n");
-                    break;
-                default:
-                    other.append(entry.getKey());
-                    other.append("=");
-                    other.append(entry.getValue());
-                    other.append("\n");
+            if(knownIdx.containsKey(entry.getKey())) {
+                bldr.append(knownIdx.get(entry.getKey()));
+                bldr.append(entry.getValue());
+                bldr.append("\n");
+            } else {
+                other.append(entry.getKey());
+                other.append("=");
+                other.append(entry.getValue());
+                other.append("\n");
             }
         }
 
@@ -122,81 +102,45 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void onDoLogin(String pos) {
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-        String url = prefs.getString("target_url", null);
-        String device_id = prefs.getString("device_id", "");
-        String group_id = prefs.getString("group_id", "");
-
-        if(url == null || url.trim().isEmpty()) {
-            Toast.makeText(this, "No URL set!", Toast.LENGTH_LONG).show();
-            Intent intent = new Intent(this, SettingsActivity.class);
-            startActivity(intent);
-            return;
-        }
-
-        String jsonText;
+        tagData.put("position", pos);
 
         try {
-            JSONObject json = new JSONObject();
-            JSONObject tag_data = new JSONObject();
-            json.put("device_id", device_id);
-            json.put("group_id", group_id);
-            json.put("position", pos);
-            json.put("action", "login");
-
-            for (Map.Entry<String, String> entry : tagData.entrySet()) {
-                tag_data.put(entry.getKey(), entry.getValue());
-            }
-
-            json.put("tag_data", tag_data);
-
-            jsonText = json.toString();
-        } catch (JSONException e) {
-            Toast.makeText(this, "JSONException", Toast.LENGTH_LONG).show();
-            return;
+            httpManager.doRequest("login", tagData);
+        } catch(HttpManager.HttpManagerException e) {
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+        } catch(HttpManager.MissingSettingException e) {
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
         }
-
-        postJson(url, jsonText);
     }
 
-    void postJson(String url, String json) {
-        RequestBody body = RequestBody.create(JSON, json);
-        Request req = new Request.Builder()
-                .url(url)
-                .post(body)
-                .build();
+    @Override
+    public void onFailure(Call call, IOException e) {
+        runOnUiThread(()->
+                Toast.makeText(LoginActivity.this, "Request failed: " + e.getMessage(), Toast.LENGTH_LONG).show()
+        );
+    }
 
-        httpClient.newCall(req)
-                .enqueue(new Callback() {
-                    @Override
-                    public void onFailure(Call call, IOException e) {
-                        runOnUiThread(()->
-                            Toast.makeText(LoginActivity.this, "Request failed", Toast.LENGTH_LONG).show()
-                        );
-                    }
+    @Override
+    public void onResponse(Call call, Response response) throws IOException {
+        ResponseBody body = response.body();
+        if(body == null)
+            return;
 
-                    @Override
-                    public void onResponse(Call call, Response response) throws IOException {
-                        ResponseBody body = response.body();
-                        if(body == null)
-                            return;
+        String res = body.string().trim();
+        int code = response.code();
 
-                        String res = body.string().trim();
-                        int code = response.code();
-
-                        if(code != 200) {
-                            runOnUiThread(()->
-                                Toast.makeText(LoginActivity.this, "Error " + code + ": " + res, Toast.LENGTH_LONG).show()
-                            );
-                            return;
-                        }
-
-                        runOnUiThread(()->{
-                            Toast.makeText(LoginActivity.this, res, Toast.LENGTH_LONG).show();
-                            backToMain();
-                        });
-                    }
-                });
+        if(code != 200) {
+            runOnUiThread(()->
+                    Toast.makeText(LoginActivity.this, "Error " + code + ": " + res, Toast.LENGTH_LONG).show()
+            );
+        } else {
+            runOnUiThread(()->{
+                Toast.makeText(LoginActivity.this, res, Toast.LENGTH_LONG).show();
+                backToMain();
+            });
+        }
     }
 
     private void backToMain() {
