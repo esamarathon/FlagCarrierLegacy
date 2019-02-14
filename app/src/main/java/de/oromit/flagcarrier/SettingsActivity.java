3a9da5ae78a5ac3c589807dc5d044849ff047398
BtbN <btbn@btbn.de>
2019-02-14 20:33:32 +0100

    Add keypair preferences

--- app/src/main/java/de/oromit/flagcarrier/SettingsActivity.java
+++ app/src/main/java/de/oromit/flagcarrier/SettingsActivity.java
@@ -1,10 +1,14 @@
 package de.oromit.flagcarrier;
 
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.preference.PreferenceFragmentCompat;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.util.Base64;
 
 public class SettingsActivity extends AppCompatActivity {
     @Override
@@ -22,6 +26,32 @@ public class SettingsActivity extends AppCompatActivity {
             setHasOptionsMenu(false);
 
             setPreferencesFromResource(R.xml.preferences, rootKey);
+
+            findPreference("gen_skpk").setOnPreferenceClickListener(preference -> {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                builder.setMessage("Overwrite existing keypair?");
+                builder.setPositiveButton("Yes", (d, p) -> {genNewKeypair(); d.dismiss();});
+                builder.setNegativeButton("No", (d, p) -> d.dismiss());
+
+                AlertDialog dialog = builder.create();
+                dialog.show();
+
+                return true;
+            });
+        }
+
+        private void genNewKeypair() {
+            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getContext());
+            SharedPreferences.Editor edit = prefs.edit();
+
+            CryptoManager.KeyPair keyPair = CryptoManager.genKeyPair();
+
+            edit.putString("priv_key", Base64.encodeToString(keyPair.PrivateKey, Base64.NO_WRAP));
+            edit.putString("pub_key", Base64.encodeToString(keyPair.PublicKey, Base64.NO_WRAP));
+
+            edit.apply();
+
+            getActivity().onBackPressed();
         }
 
         @Override
